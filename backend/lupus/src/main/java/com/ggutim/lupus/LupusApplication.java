ed28f4fbc1a6dd26dd9a34f06f9a85c8a559c190
Mattia Gualtieri <mattiagualtieri2000@gmail.com>
2026-08-13 15:06:46 +0200

    Backend initialization

---
+++ backend/lupus/src/main/java/com/ggutim/lupus/LupusApplication.java
@@ -0,0 +1,13 @@
+package com.ggutim.lupus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LupusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LupusApplication.class, args);
+	}
+
+}
---
+++ backend/lupus/src/test/java/com/ggutim/lupus/LupusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ggutim.lupus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LupusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
